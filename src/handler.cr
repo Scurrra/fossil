1ccc2888c0b6d10d541acd1672f8ea6df92afbfd
Scurrra <iscurrra@gmail.com>
2025-08-11 00:33:52 +0300

    String parameter bug + Form fix

--- src/handler.cr
+++ src/handler.cr
@@ -3,6 +3,8 @@ require "./params"
 require "./methods"
 require "./errors"
 
+require "http/headers"
+
 macro method_added(endpoint_fun)
   {% for m in {GET, POST, PUT, HEAD, DELETE, PATCH, OPTIONS} %}     
     {% for m_ in endpoint_fun.annotations(m) %}
@@ -15,10 +17,21 @@ macro method_added(endpoint_fun)
         end
     
         def call(context : HTTP::Server::Context, path_params : Hash(String, Fossil::Param::PathParamType))
-          form_data = {} of String => HTTP::FormData::Part
-          if context.request.body
-            HTTP::FormData.parse(context.request) do |part|
-              form_data[part.name] = part
+          form_data = {} of String => String
+          file_data = {} of String => File
+          if context.request.headers.has_key? "Content-Type"
+            if context.request.headers.includes_word?("Content-Type", "multipart/form-data")
+              HTTP::FormData.parse(context.request) do |part|
+                if part.headers.includes_word?("Content-Disposition", "filename")
+                  if filename = part.filename
+                    file_data[part.name] = File.tempfile(filename) do |file|
+                      IO.copy(part.body, file)
+                    end
+                  end
+                else
+                  form_data[part.name] = part.body.gets_to_end
+                end
+              end
             end
           end
       
@@ -39,6 +52,19 @@ macro method_added(endpoint_fun)
               
               begin
               {% if query_ann_shadowed.named_args.has_key?(:name) && query_ann_shadowed.named_args.has_key?(:alias) %}
+              
+                {% if arg.restriction.stringify == "String" %}
+                
+                if context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:name]}})
+                  {{arg.internal_name.id}} = context.request.query_params[{{arg.annotation(Fossil::Param::Query)[:name]}}]
+                elsif context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:alias]}})
+                  {{arg.internal_name.id}} = context.request.query_params[{{arg.annotation(Fossil::Param::Query)[:alias]}}]
+                else
+                  {{arg.internal_name.id}} = context.request.query_params[{{arg.name.stringify}}]
+                end
+              
+                {% else %}
+
                 if context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:name]}})
                   {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.query_params[{{arg.annotation(Fossil::Param::Query)[:name]}}]
                 elsif context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:alias]}})
@@ -46,25 +72,65 @@ macro method_added(endpoint_fun)
                 else
                   {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.query_params[{{arg.name.stringify}}]
                 end
+              
+                {% end %}
+            
               {% elsif query_ann_shadowed.named_args.has_key?(:name) %}
+              
+                {% if arg.restriction.stringify == "String" %}
+                
+                if context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:name]}})
+                  {{arg.internal_name.id}} = context.request.query_params[{{arg.annotation(Fossil::Param::Query)[:name]}}]
+                else
+                  {{arg.internal_name.id}} = context.request.query_params[{{arg.name.stringify}}]
+                end
+              
+                {% else %}
+
                 if context.request.query_params.has_key?({{arg.annotation(Fossil::Param::Query)[:name]}})
                   {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.query_params[{{arg.annotation(Fossil::Param::Query)[:name]}}]
                 else
                   {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.query_params[{{arg.name.stringify}}]
                 end
+              
+                {% end %}
+            
               {% else %}
+              
+                {% if arg.restriction.stringify == "String" %}
+                
+                {{arg.internal_name.id}} = context.request.query_params[{{arg.name.stringify}}]
+              
+                {% else %}
+
                 {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.query_params[{{arg.name.stringify}}]
+
+                {% end %}
+            
               {% end %}
               rescue
                 raise Fossil::Error::ParamParseError.new "Cannot parse query parameter #{{{arg.name.stringify}}}."
               end
             
             {% elsif form_ann_shadowed = arg.annotation(Fossil::Param::Form) %}
-
+        
               if form_params = context.request.form_params?
                 
                 begin
                 {% if form_ann_shadowed.named_args.has_key?(:name) && form_ann_shadowed.named_args.has_key?(:alias) %}
+          
+                  {% if arg.restriction.stringify == "String" %}
+            
+                  if context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
+                    {{arg.internal_name.id}} = context.request.form_params[{{arg.annotation(Fossil::Param::Form)[:name]}}]
+                  elsif context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:alias]}})
+                    {{arg.internal_name.id}} = context.request.form_params[{{arg.annotation(Fossil::Param::Form)[:alias]}}]
+                  else
+                    {{arg.internal_name.id}} = context.request.form_params[{{arg.name.stringify}}]
+                  end
+            
+                  {% else %}
+            
                   if context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
                     {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.form_params[{{arg.annotation(Fossil::Param::Form)[:name]}}]
                   elsif context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:alias]}})
@@ -72,23 +138,64 @@ macro method_added(endpoint_fun)
                   else
                     {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.form_params[{{arg.name.stringify}}]
                   end
+            
+                  {% end %}
+            
                 {% elsif form_ann_shadowed.named_args.has_key?(:name) %}
+          
+                  {% if arg.restriction.stringify == "String" %}
+            
+                  if context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
+                    {{arg.internal_name.id}} = context.request.form_params[{{arg.annotation(Fossil::Param::Form)[:name]}}]
+                  else
+                    {{arg.internal_name.id}} = context.request.form_params[{{arg.name.stringify}}]
+                  end
+            
+                  {% else %}
+            
                   if context.request.form_params.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
                     {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.form_params[{{arg.annotation(Fossil::Param::Form)[:name]}}]
                   else
                     {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.form_params[{{arg.name.stringify}}]
                   end
+            
+                  {% end %}
+            
                 {% else %}
+          
+                  {% if arg.restriction.stringify == "String" %}
+            
+                  {{arg.internal_name.id}} = context.request.form_params[{{arg.name.stringify}}]
+            
+                  {% else %}
+            
                   {{arg.internal_name.id}} = {{arg.restriction}}.new context.request.form_params[{{arg.name.stringify}}]
+            
+                  {% end %}
+            
                 {% end %}
                 rescue
                   raise Fossil::Error::ParamParseError.new "Cannot parse form parameter #{{{arg.name.stringify}}}."
                 end
 
               else
-              
+                
                 begin
+                
                 {% if form_ann_shadowed.named_args.has_key?(:name) && form_ann_shadowed.named_args.has_key?(:alias) %}
+              
+                {% if arg.restriction.stringify == "String" %}
+              
+                  if form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
+                    {{arg.internal_name.id}} = form_data[{{arg.annotation(Fossil::Param::Form)[:name]}}]
+                  elsif form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:alias]}})
+                    {{arg.internal_name.id}} = form_data[{{arg.annotation(Fossil::Param::Form)[:alias]}}]
+                  else
+                    {{arg.internal_name.id}} = form_data[{{arg.name.stringify}}]
+                  end
+                
+                {% else %}
+                  
                   if form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
                     {{arg.internal_name.id}} = {{arg.restriction}}.new form_data[{{arg.annotation(Fossil::Param::Form)[:name]}}]
                   elsif form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:alias]}})
@@ -96,15 +203,39 @@ macro method_added(endpoint_fun)
                   else
                     {{arg.internal_name.id}} = {{arg.restriction}}.new form_data[{{arg.name.stringify}}]
                   end
+                
+                {% end %}
+                
                 {% elsif form_ann_shadowed.named_args.has_key?(:name) %}
+                
+                {% if arg.restriction.stringify == "String" %}
+                
+                  if form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
+                    {{arg.internal_name.id}} = form_data[{{arg.annotation(Fossil::Param::Form)[:name]}}]
+                  else
+                    {{arg.internal_name.id}} = form_data[{{arg.name.stringify}}]
+                  end
+                
+                {% else %}
+                  
                   if form_data.has_key?({{arg.annotation(Fossil::Param::Form)[:name]}})
                     {{arg.internal_name.id}} = {{arg.restriction}}.new form_data[{{arg.annotation(Fossil::Param::Form)[:name]}}]
                   else
                     {{arg.internal_name.id}} = {{arg.restriction}}.new form_data[{{arg.name.stringify}}]
                   end
+                
+                {% end %}
+
+                {% else %}
+                
+                {% if arg.restriction.stringify == "String" %}
+                  {{arg.internal_name.id}} = form_data[{{arg.name.stringify}}]
                 {% else %}
                   {{arg.internal_name.id}} = {{arg.restriction}}.new form_data[{{arg.name.stringify}}]
                 {% end %}
+
+                {% end %}
+
                 rescue
                   raise Fossil::Error::ParamParseError.new "Cannot parse form parameter #{{{arg.name.stringify}}}."
                 end
@@ -115,18 +246,14 @@ macro method_added(endpoint_fun)
 
               begin
               {% if file_ann_shadowed.named_args.has_key?(:name) %}
-                {{arg.internal_name.id}} = if form_data.has_key?({{arg.annotation(Fossil::Param::File)[:name]}})
-                  File.tempfile(form_data[{{arg.annotation(Fossil::Param::File)[:name]}}].filename || "upload") do |tmpfile|
-                    IO.copy(form_data[{{arg.annotation(Fossil::Param::File)[:name]}}].body, file)
-                  end
+                {{arg.internal_name.id}} = if file_data.has_key?({{arg.annotation(Fossil::Param::File)[:name]}})
+                  file_data[{{arg.annotation(Fossil::Param::File)[:name]}}]
                 else
                   nil
                 end
               {% else %}
-                {{arg.internal_name.id}} = if form_data.has_key?({{arg.name.stringify}})
-                  File.tempfile(form_data[{{arg.name.stringify}}].filename || "upload") do |tmpfile|
-                    IO.copy(form_data[{{arg.name.stringify}}].body, file)
-                  end
+                {{arg.internal_name.id}} = if file_data.has_key?({{arg.name.stringify}})
+                  file_data[{{arg.name.stringify}}]
                 else
                   nil
                 end
